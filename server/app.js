bb405faffeaf5d702fbaedc346a1aee4b51fdd6f
Andres3436 <124109273+Andres3436@users.noreply.github.com>
2023-04-19 15:06:26 +0000

    😎Installing Winston

--- server/app.js
+++ server/app.js
@@ -1,15 +1,15 @@
 // Helps to handle http errors
-import createError from 'http-errors'
+import createError from 'http-errors';
 // Import the Express Library
 import express from 'express';
 // Is a Core-Node library to manage system paths
-import path from 'path'
+import path from 'path';
 // Helps to parse client cookies
 import cookieParser from 'cookie-parser';
 // Library to log http communication
-import logger from 'morgan'
+import morgan from 'morgan';
 
-import indexRouter from '@server/routes/index' 
+import indexRouter from '@server/routes/index';
 import usersRouter from '@server/routes/users';
 import apiRouter from '@server/routes/api';
 
@@ -17,41 +17,40 @@ import apiRouter from '@server/routes/api';
 import webpack from 'webpack';
 import WebpackDevMiddleware from 'webpack-dev-middleware';
 import WebpackHotMiddleware from 'webpack-hot-middleware';
-
 // Importin webpack configuration
 import webpackConfig from '../webpack.dev.config';
-import WebpackDevMiddleware from 'webpack-dev-middleware';
-import WebpackHotMiddleware from 'webpack-hot-middleware';
 
 // We are creating the express instance
 const app = express();
 
-//Get the execution mode
-const nodeEnviroment = process.env.NODE_ENV  || 'production'
+// Get the execution mode
+const nodeEnviroment = process.env.NODE_ENV || 'production';
 
 // Deciding if we add webpack middleware or not
-if(nodeEnviroment === 'development'){
+if (nodeEnviroment === 'development') {
   // Start Webpack dev server
-  console.log("🦿Ejecutando en modo desarrollo");
+  console.log('🦿Ejecutando en modo desarrollo');
   // Adding the key "mode" with its value "development"
   webpackConfig.mode = nodeEnviroment;
   // Setting the port
   webpackConfig.devServer.port = process.env.PORT;
   // Setting up the HMR (Hot Module Replacement)
   webpackConfig.entry = [
-  "webpack-hot-middleware/client?reload=true&timeout=1000", 
-  webpackConfig.entry
-];
-// Cretaing the bundler
-const bundler = webpack(webpackConfig);
-// Enabling the webpack middleware
-app.use( WebpackDevMiddleware(bundler, {
-  publicPath: webpackConfig.output.publicPath
-}));
-// Enabling the webpack HMR
-app.use( WebpackHotMiddleware (bundle));
-}else{
-  console.log("🐱‍👤Ejecutando en modo produccion");
+    'webpack-hot-middleware/client?reload=true&timeout=1000',
+    webpackConfig.entry,
+  ];
+  // Cretaing the bundler
+  const bundler = webpack(webpackConfig);
+  // Enabling the webpack middleware
+  app.use(
+    WebpackDevMiddleware(bundler, {
+      publicPath: webpackConfig.output.publicPath,
+    })
+  );
+  // Enabling the webpack HMR
+  app.use(WebpackHotMiddleware(bundler));
+} else {
+  console.log('🐱‍👤Ejecutando en modo produccion');
 }
 
 // view engine setup
@@ -62,7 +61,7 @@ app.set('view engine', 'hbs');
 
 // Registering middlewares
 // Log all received request
-app.use(logger('dev'));
+app.use(morgan('dev'));
 // Parse request data into json
 app.use(express.json());
 // Decode the url info
@@ -75,15 +74,15 @@ app.use(express.static(path.join(__dirname, '../public')));
 // Registering routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api',apiRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -93,4 +92,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
