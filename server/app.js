3f17680a14e242152002fc6335718b2d8d778547
Andres3436 <124109273+Andres3436@users.noreply.github.com>
2023-05-08 14:18:57 +0000

    👻Dominio-Home

--- server/app.js
+++ server/app.js
@@ -9,9 +9,8 @@ import cookieParser from 'cookie-parser';
 // Library to log http communication
 import morgan from 'morgan';
 
-import indexRouter from '@server/routes/index';
-import usersRouter from '@server/routes/users';
-import apiRouter from '@server/routes/api';
+// Importing template-engine
+import configTemplateEngine from '@server/config/templateEngine';
 
 // Setting Webpack Modules
 import webpack from 'webpack';
@@ -20,11 +19,11 @@ import WebpackHotMiddleware from 'webpack-hot-middleware';
 // Importing webpack configuration
 import webpackConfig from '../webpack.dev.config';
 
-// Importando el configurador del motor de plantillas
-import configTemplateEngine from './config/templateEngine';
-
 // Impornting winston logger
 import log from './config/winston';
+
+// Importando enrutador
+import router from './router';
 
 // Creando variable del directorio raiz
 // eslint-disable-next-line
@@ -79,9 +78,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Registering routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', apiRouter);
+router.addRoutes(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
