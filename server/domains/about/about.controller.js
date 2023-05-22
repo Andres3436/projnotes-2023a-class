d84f3e44276ede9bfdf2885ca882fd90c5689b21
Andres3436 <124109273+Andres3436@users.noreply.github.com>
2023-05-22 14:43:39 +0000

    ✨About

--- server/domains/about/about.controller.js
+++ server/domains/about/about.controller.js
@@ -1,6 +1,9 @@
 // Actions methods
 // GET "/about"
 const about = (req, res) => {
+  const iconSet = ['⭐', '🤖', '🍉', '🏎', '🦼', '🚨', '🍔', '🎭'];
+  const icon = iconSet[Math.floor(Math.random() * iconSet.length)];
+  res.render('about/aboutView', { icon });
   res.send('👻 UNDER CONSTRUCTION: GET/about');
 };
 
--- server/domains/home/home.controller.js
+++ server/domains/home/home.controller.js
@@ -2,9 +2,9 @@
 // GET "/"
 // GET "/index"
 const home = (req, res) => {
-  const iconSet = ['⭐', '🤖', '🍉'];
-  const icon = iconSet[Math.floor(Math.random() * 3)];
-  res.render('index', { title: 'DWPCII-2023A', icon });
+  const iconSet = ['⭐', '🤖', '🍉', '🏎', '🦼', '🚨', '🍔', '🎭'];
+  const icon = iconSet[Math.floor(Math.random() * iconSet.length)];
+  res.render('home/homeView', { icon });
 };
 
 // GET "/index"
