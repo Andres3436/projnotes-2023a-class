1e69f6363bf2cd16c9a18b3c4217284edd164b48
Andres3436 <124109273+Andres3436@users.noreply.github.com>
2023-05-29 15:43:44 +0000

    🥺Modelo Proyecto

---
+++ server/domains/project/project.controller.js
@@ -0,0 +1,18 @@
+// Actions methods
+// GET "/project"
+const showDashboard = (req, res) => {
+    res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
+  };
+  
+  // GET "/project/add"
+  const add = (req, res) => {
+    res.render('project/addView');
+  };
+  
+  // Controlador user
+  export default {
+    // Action Methods
+    showDashboard,
+    add,
+  };
+  
